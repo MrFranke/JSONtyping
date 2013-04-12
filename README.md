5e595ce30c84ed41981088bdf6db79a56b2fb431
mmoskalev.dv.rbc.ru user <mmoskalev@mmoskalev.dv.rbc.ru>
2013-04-12 12:18:08 +0400

    Add JSON formatting

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 JSONtyping
 ==========
 
-***Здесь будет какое то описание***
+Функция заменяет зачение свойств обхекта JSON-a на их типы:
+
+Исходный JSON:
+```javascript
+{"testString": "bla-bla-bla", "testObj": {"testArray":["this", "is", "text"], "emptyObj":{}, "emptyArray":[]}, "thisNumber":123, "isBoolean":false}
+```
+
+Результат:
+```javascript
+{
+    "testString": "string",
+    "testObj": {
+        "testArray": [
+            "string"
+        ],
+        "emptyObj": "object",
+        "emptyArray": "object"
+    },
+    "thisNumber": "number",
+    "boolean": "boolean"
+}
+```
 
 json, typisation
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-
-function jsonTypeisation (json, maxlvl, n) {
+/**
+ * Функция проходится по всему JSON-у и заменяет значения его свойств на типы.
+ * @param json {Object} Объект JSON-а или массива, который нужно обработать
+ * @param m {Number} Максимальная глубина вхождение в JSON. Если не передавать аргумент,
+ *                        функция обработает весь JSON.
+ * @param n {Number} Необходим для определения уровня JSON-а, на котором находится обрабатываемый функцией объект
+ *
+ */
+function jsonTypeisation (json, m, n) {
    var newObj = {}
-     , nesting = n || 1;
+     , nesting = n || 1
+     , maxlvl = m || Infinity;
    if ( !Object.keys(json).length ) return 'object';
    if ( (typeof json == "object") && (json instanceof Array) ) newObj = [];  // Если json == array, вместо {} создаем []
    for( param in json ) {
@@ -21,6 +29,21 @@ function jsonTypeisation (json, maxlvl, n) {
       newObj[param] = type;
       if ( newObj.length ) return newObj; // По массиву проходимся только 1 раз
    }
-   
    return newObj;
+}
+
+
+
+
+var input = document.getElementById('input').value
+  , output = document.getElementById('output')
+  , button = document.getElementsByTagName('button')[0]
+   
+button.addEventListener('click', run);
+
+function run(){
+   input = document.getElementById('input').value;
+   input = JSON.parse( input );
+   output.innerHTML = JSON.stringify( jsonTypeisation(input, 3), null, 4 );
+   return false;
 }
